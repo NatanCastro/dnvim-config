0dcd48f97845421908c79ae235f58bd4e52e70db
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2026-04-26 20:49:34 -0300

    fix: migrate treesitter config to new api

--- lua/dnvim/plugins/treesitter.lua
+++ lua/dnvim/plugins/treesitter.lua
@@ -1,29 +1,20 @@
 return {
   {
-   'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'ocaml' },
+    config = function()
+      local ts = require('nvim-treesitter')
+      ts.setup()
 
-      auto_install = true,
-      highlight = {
-		enable = true,
-		disable = { 'latex', },
-     },
-	indent = { enable = false, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      local group = vim.api.nvim_create_augroup('dnvim_treesitter_start', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        pattern = { 'bash', 'c', 'diff', 'go', 'help', 'html', 'lua', 'markdown', 'ocaml', 'vim' },
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
     end,
   },
 }
