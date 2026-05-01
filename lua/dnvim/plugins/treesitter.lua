775f7fbd47aa6b8dfde0720adde3da3895a117fa
danielscoffee <danielrsantoscontato@gmail.com>
2026-05-01 16:48:19 -0300

    chore: neorg_meta added as tressitter parser

--- lua/dnvim/plugins/treesitter.lua
+++ lua/dnvim/plugins/treesitter.lua
@@ -7,10 +7,20 @@ return {
       local ts = require('nvim-treesitter')
       ts.setup()
 
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.norg_meta = {
+        install_info = {
+          url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+        filetype = 'norg_meta',
+      }
+
       local group = vim.api.nvim_create_augroup('dnvim_treesitter_start', { clear = true })
       vim.api.nvim_create_autocmd('FileType', {
         group = group,
-        pattern = { 'bash', 'c', 'diff', 'go', 'help', 'html', 'lua', 'markdown', 'ocaml', 'vim' },
+        pattern = { 'bash', 'c', 'diff', 'go', 'help', 'html', 'lua', 'markdown', 'norg', 'ocaml', 'vim' },
         callback = function(args)
           pcall(vim.treesitter.start, args.buf)
         end,
